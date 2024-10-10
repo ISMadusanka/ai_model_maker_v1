3b2f9cb09a38228911765508cb5f0d930c0cd7ab
Isuru Sanjeewa Madusanka <143842041+ISMadusanka@users.noreply.github.com>
2024-10-10 14:02:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.get('/set-cookies', (req, res) => {
     res.cookie('newUser', false);
     res.cookie('isEmployee', true, { maxAge: 1000 * 10, httpOnly: true });
 
-    res.send('you got cookies!');
+    res.send('you gotkies!');
 });
 
 app.get('/read-cookies', (req, res) => {
